bfa3132bd0211d783196e42320d4e0372b506759
MassimilianoT <49036204+MassimilianoT@users.noreply.github.com>
2020-05-23 15:31:13 +0200

    Create WriteJsonString.java

---
+++ WriteJsonString.java
@@ -0,0 +1,18 @@
+public class WriteJSONString {
+    public static void main(String[] args) {
+        JSONObject object = new JSONObject();
+        object.put("id", 1L);
+        object.put("name", "Alice");
+        object.put("age", 20);
+
+        JSONArray courses = new JSONArray();
+        courses.put("Engineering");
+        courses.put("Finance");
+        courses.put("Chemistry");
+
+        object.put("courses", courses);
+
+        String jsonString = object.toString(2);
+        System.out.println(jsonString);
+    }
+}
